9c744a7049b3adcb1beaaa630949ca223c8be374
Erika Orrego Giraldo <erikaorrego16@gmail.com>
2022-09-18 23:09:38 -0500

    Erikas pipe

---
+++ src/app/shared/pipes/change-weight.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { ChangeWeightPipe } from './change-weight.pipe';
+
+describe('ChangeWeightPipe', () => {
+  it('create an instance', () => {
+    const pipe = new ChangeWeightPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -6,8 +6,9 @@ import { MaterialModule } from '../material/material.module';
 import { ChangeNamePipe } from './pipes/change-name.pipe';
 import { DirectiveSearchDirective } from './directives/directive-search.directive';
 import { IndexTablePipe } from './pipes/index-table.pipe';
+import { ChangeWeightPipe } from './pipes/change-weight.pipe';
 
-const COMPONENTS = [FooterComponent,HeaderComponent,ChangeNamePipe,IndexTablePipe,DirectiveSearchDirective,IndexTablePipe]
+const COMPONENTS = [FooterComponent,HeaderComponent,ChangeNamePipe,IndexTablePipe,DirectiveSearchDirective,IndexTablePipe,ChangeWeightPipe]
 
 @NgModule({
   declarations: [...COMPONENTS],
