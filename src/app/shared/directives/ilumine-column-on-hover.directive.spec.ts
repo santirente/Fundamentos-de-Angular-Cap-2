a8725ad1ccf88e194b5e72a74e8be0e25cc4868e
hernandoponzon <hernandonunez13@gmail.com>
2022-09-22 00:22:19 -0500

    Se creo una directiva para iluminar las filas cuando se les hace hover

---
+++ src/app/shared/directives/ilumine-column-on-hover.directive.spec.ts
@@ -0,0 +1,8 @@
+import { IlumineColumnOnHoverDirective } from './ilumine-column-on-hover.directive';
+
+describe('IlumineColumnOnHoverDirective', () => {
+  it('should create an instance', () => {
+    const directive = new IlumineColumnOnHoverDirective();
+    expect(directive).toBeTruthy();
+  });
+});
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -7,11 +7,12 @@ import { ChangeNamePipe } from './pipes/change-name.pipe';
 import { DirectiveSearchDirective } from './directives/directive-search.directive';
 import { IndexTablePipe } from './pipes/index-table.pipe';
 import { OrderByWeightPipe } from './pipes/order-by-weight.pipe';
+import { IlumineColumnOnHoverDirective } from './directives/ilumine-column-on-hover.directive';
 
-const COMPONENTS = [FooterComponent,HeaderComponent,ChangeNamePipe,IndexTablePipe,DirectiveSearchDirective,IndexTablePipe, OrderByWeightPipe]
+const COMPONENTS = [FooterComponent,HeaderComponent,ChangeNamePipe,IndexTablePipe,DirectiveSearchDirective,IndexTablePipe, OrderByWeightPipe, IlumineColumnOnHoverDirective];
 
 @NgModule({
-  declarations: [...COMPONENTS, OrderByWeightPipe],
+  declarations: [...COMPONENTS, OrderByWeightPipe, IlumineColumnOnHoverDirective],
   imports: [
     CommonModule,
     MaterialModule
