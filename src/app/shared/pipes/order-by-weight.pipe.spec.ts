4a6bc2002081fd577a8ac46f05a65814ed698a72
hernandoponzon <hernandonunez13@gmail.com>
2022-09-21 23:12:14 -0500

    Se creo un pipe para ordenar elementos periodicos con respecto a su masa

---
+++ src/app/shared/pipes/order-by-weight.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { OrderByWeightPipe } from './order-by-weight.pipe';
+
+describe('OrderByWeightPipe', () => {
+  it('create an instance', () => {
+    const pipe = new OrderByWeightPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -6,11 +6,12 @@ import { MaterialModule } from '../material/material.module';
 import { ChangeNamePipe } from './pipes/change-name.pipe';
 import { DirectiveSearchDirective } from './directives/directive-search.directive';
 import { IndexTablePipe } from './pipes/index-table.pipe';
+import { OrderByWeightPipe } from './pipes/order-by-weight.pipe';
 
-const COMPONENTS = [FooterComponent,HeaderComponent,ChangeNamePipe,IndexTablePipe,DirectiveSearchDirective,IndexTablePipe]
+const COMPONENTS = [FooterComponent,HeaderComponent,ChangeNamePipe,IndexTablePipe,DirectiveSearchDirective,IndexTablePipe, OrderByWeightPipe]
 
 @NgModule({
-  declarations: [...COMPONENTS],
+  declarations: [...COMPONENTS, OrderByWeightPipe],
   imports: [
     CommonModule,
     MaterialModule
